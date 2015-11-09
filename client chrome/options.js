c85b584fb8b2e2a6dbf9ad6038d978d3a2f970a2
Сельченков Николай <undwad@integra-s.com>
2015-11-09 19:08:27 +0400

    +

--- client chrome/options.js
+++ client chrome/options.js
@@ -1,37 +1,12 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var gmail = "https://mail.google.com/mail/?extsrc=mailto&url=%s";
-
-function toggle(radioButton) {
-  if (window.localStorage == null) {
-    alert('Local storage is required for changing providers');
-    return;
-  }
-  if (document.getElementById('gmail').checked) {
-    window.localStorage.customMailtoUrl = gmail;
-  } else {
-    window.localStorage.customMailtoUrl = "";
-  }
-}
-
-function main() {
-  if (window.localStorage == null) {
-    alert("LocalStorage must be enabled for changing options.");
-    document.getElementById('default').disabled = true;
-    document.getElementById('gmail').disabled = true;
-    return;
-  }
-
-  // Default handler is checked. If we've chosen another provider, we must
-  // change the checkmark.
-  if (window.localStorage.customMailtoUrl == gmail)
-    document.getElementById('gmail').checked = true;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  main();
-  document.querySelector('#default').addEventListener('click', toggle);
-  document.querySelector('#gmail').addEventListener('click', toggle);
+$(function ()
+{
+    if (!window.localStorage.port) window.localStorage.port = 2711
+    var host = $('#host')
+    var port = $('#port')
+    host.val(window.localStorage.host || '')
+    port.val(window.localStorage.port || '')
+    host.change(function () { window.localStorage.host = host.val() })
+    port.change(function () { window.localStorage.port = port.val() })
+    host.keyup(function () { window.localStorage.host = host.val() })
+    port.keyup(function () { window.localStorage.port = port.val() })
 });
